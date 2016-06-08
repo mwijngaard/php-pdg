21c8126befa775312f9df260d5925f537bf19055
Merijn Wijngaard <mwijngaard@gmail.com>
2016-06-08 14:14:07 +0200

    attempt to optimize getEdges and hasEdges by indexing by from and to node

--- src/PhpPdg/Graph/Graph.php
+++ src/PhpPdg/Graph/Graph.php
@@ -7,8 +7,10 @@ use PhpPdg\Graph\Node\NodeInterface;
 class Graph implements GraphInterface {
 	/** @var NodeInterface[] */
 	private $nodes = [];
-	/** @var Edge[] */
-	private $edges = [];
+	/** @var Edge[][][] */
+	private $outgoing_edges = [];
+	/** @var Edge[][][] */
+	private $incoming_edges = [];
 
 	public function addNode(NodeInterface $node) {
 		$hash = $node->getHash();
@@ -19,9 +21,14 @@ class Graph implements GraphInterface {
 	}
 
 	public function addEdge(NodeInterface $from_node, NodeInterface $to_node, array $attributes = []) {
-		$this->assertNodeExistsIfNotNull('from_node', $from_node);
-		$this->assertNodeExistsIfNotNull('to_node', $to_node);
-		$this->edges[] = new Edge($from_node, $to_node, $attributes);
+		if ($this->hasEdges($from_node, $to_node, $attributes, true) === true) {
+			throw new \InvalidArgumentException("Edge already exists");
+		}
+		$edge = new Edge($from_node, $to_node, $attributes);
+		$from_node_hash = $from_node->getHash();
+		$to_node_hash = $to_node->getHash();
+		$this->outgoing_edges[$from_node_hash][$to_node_hash][] = $edge;
+		$this->incoming_edges[$to_node_hash][$from_node_hash][] = $edge;
 	}
 
 	public function hasNode(NodeInterface $node) {
@@ -31,12 +38,7 @@ class Graph implements GraphInterface {
 	public function hasEdges(NodeInterface $from_node = null, NodeInterface $to_node = null, array $filterAttributes = [], $filterAttributesExact = false) {
 		$this->assertNodeExistsIfNotNull('from_node', $from_node);
 		$this->assertNodeExistsIfNotNull('to_node', $to_node);
-		foreach ($this->edges as $edge) {
-			if ($this->edgeMatches($edge, $from_node, $to_node, $filterAttributes, $filterAttributesExact) === true) {
-				return true;
-			}
-		}
-		return false;
+		return $this->hasEdgesRecursive($this->getEdgeCollection($from_node, $to_node), $filterAttributes, $filterAttributesExact);
 	}
 
 	public function getNodes() {
@@ -47,52 +49,128 @@ class Graph implements GraphInterface {
 		$this->assertNodeExistsIfNotNull('from_node', $from_node);
 		$this->assertNodeExistsIfNotNull('to_node', $to_node);
 		$result = [];
-		foreach ($this->edges as $edge) {
-			if ($this->edgeMatches($edge, $from_node, $to_node, $filterAttributes, $filterAttributesExact) === true) {
-				$result[] = $edge;
-			}
-		}
+		$this->addEdgesRecursive($result, $this->getEdgeCollection($from_node, $to_node), $filterAttributes, $filterAttributesExact);
 		return $result;
 	}
 
 	public function deleteNode(NodeInterface $node) {
 		$this->assertNodeExistsIfNotNull($node, 'node');
-		foreach ($this->edges as $index => $edge) {
-			if ($this->edgeMatches($edge, $node) === true || $this->edgeMatches($edge, null, $node) === true) {
-				unset($this->edges[$index]);
-			}
-		}
+		$this->deleteEdges($node);
+		$this->deleteEdges(null, $node);
 		unset($this->nodes[$node->getHash()]);
 	}
 
 	public function deleteEdges(NodeInterface $from_node = null, NodeInterface $to_node = null, array $filterAttributes = [], $filterAttributesExact = false) {
 		$this->assertNodeExistsIfNotNull('from_node', $from_node);
 		$this->assertNodeExistsIfNotNull('to_node', $to_node);
-		foreach ($this->edges as $index => $edge) {
-			if ($this->edgeMatches($edge, $from_node, $to_node, $filterAttributes, $filterAttributesExact) === true) {
-				unset($this->edges[$index]);
+		foreach ($this->getEdges($from_node, $to_node, $filterAttributes, $filterAttributesExact) as $edge) {
+			$from_node_hash = $from_node->getHash();
+			$to_node_hash = $to_node->getHash();
+			$from_outgoing_edges = &$this->outgoing_edges[$from_node_hash];
+			$from_to_outgoing_edges = &$from_outgoing_edges[$to_node_hash];
+			unset($from_to_outgoing_edges[array_search($edge, $from_to_outgoing_edges, true)]);
+			if (empty($from_to_outgoing_edges) === true) {
+				unset($from_to_outgoing_edges[$to_node_hash]);
+			}
+			if (empty($from_outgoing_edges) === true) {
+				unset($this->outgoing_edges[$from_node_hash]);
+			}
+			$to_incoming_edges = &$this->incoming_edges[$to_node_hash];
+			$to_from_incoming_edges = &$to_incoming_edges[$from_node_hash];
+			unset($to_from_incoming_edges[array_search($edge, $to_from_incoming_edges, true)]);
+			if (empty($to_from_incoming_edges) === true) {
+				unset($to_incoming_edges[$from_node_hash]);
+			}
+			if (empty($to_incoming_edges) === true) {
+				unset($this->incoming_edges[$to_node_hash]);
 			}
 		}
 	}
 
 	public function clear() {
 		$this->nodes = [];
-		$this->edges = [];
+		$this->outgoing_edges = [];
+		$this->incoming_edges = [];
 	}
 
+	/**
+	 * @param $label
+	 * @param NodeInterface|null $node
+	 * @throws \InvalidArgumentException
+	 */
 	private function assertNodeExistsIfNotNull($label, NodeInterface $node = null) {
 		if ($node !== null && isset($this->nodes[$node->getHash()]) === false) {
 			throw new \InvalidArgumentException(sprintf("Node `%s` does not exist", $label));
 		}
 	}
 
-	private function edgeMatches(Edge $edge, NodeInterface $from_node = null, NodeInterface $to_node = null, array $filterAttributes = [], $filterAttributesExact = false) {
-		return
-			($from_node === null || $from_node->equals($edge->getFromNode()))
-			&& ($to_node === null || $to_node->equals($edge->getToNode()))
-			&& $this->attributesMatch($edge->getAttributes(), $filterAttributes, $filterAttributesExact);
+	/**
+	 * @param NodeInterface|null $from_node
+	 * @param NodeInterface|null $to_node
+	 * @return Edge[]|Edge[][]|Edge[][][]
+	 */
+	private function getEdgeCollection(NodeInterface $from_node = null, NodeInterface $to_node = null) {
+		if ($from_node !== null && $to_node !== null) {
+			$from_node_hash = $from_node->getHash();
+			$to_node_hash = $to_node->getHash();
+			return isset($this->outgoing_edges[$from_node_hash][$to_node_hash]) === true ? $this->outgoing_edges[$from_node_hash][$to_node_hash] : [];
+		} else if ($from_node !== null) {
+			$from_node_hash = $from_node->getHash();
+			return isset($this->outgoing_edges[$from_node_hash]) === true ? $this->outgoing_edges[$from_node_hash] : [];
+		} else if ($to_node !== null) {
+			$to_node_hash = $to_node->getHash();
+			return isset($this->incoming_edges[$to_node_hash]) === true ? $this->incoming_edges[$to_node_hash] : [];
+		}
+		return $this->outgoing_edges;
 	}
 
+	/**
+	 * @param Edge[]|Edge[][]|Edge[][][] $edge_collection
+	 * @param array $filterAttributes
+	 * @param $filterAttributesExact
+	 * @return bool
+	 */
+	private function hasEdgesRecursive($edge_collection, array $filterAttributes, $filterAttributesExact) {
+		foreach ($edge_collection as $edge_collection_entry) {
+			if (is_array($edge_collection_entry) === true) {
+				if ($this->hasEdgesRecursive($edge_collection_entry, $filterAttributes, $filterAttributesExact) === true) {
+					return true;
+				}
+			} else {
+				assert($edge_collection_entry instanceof Edge);
+				if ($this->attributesMatch($edge_collection_entry->getAttributes(), $filterAttributes, $filterAttributesExact) === true) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * @param array $result
+	 * @param Edge[]|Edge[][]|Edge[][][] $edge_collection
+	 * @param array $filterAttributes
+	 * @param $filterAttributesExact
+	 */
+	private function addEdgesRecursive(array &$result, $edge_collection, array $filterAttributes, $filterAttributesExact) {
+		foreach ($edge_collection as $edge_collection_entry) {
+			if (is_array($edge_collection_entry) === true) {
+				$this->addEdgesRecursive($result, $edge_collection_entry, $filterAttributes, $filterAttributesExact);
+			} else {
+				assert($edge_collection_entry instanceof Edge);
+				if ($this->attributesMatch($edge_collection_entry->getAttributes(), $filterAttributes, $filterAttributesExact) === true) {
+					$result[] = $edge_collection_entry;
+				}
+			}
+		}
+	}
+
+	/**
+	 * @param array $attributes
+	 * @param array $filter_attributes
+	 * @param $filter_attributes_exact
+	 * @return bool
+	 */
 	private function attributesMatch(array $attributes, array $filter_attributes, $filter_attributes_exact) {
 		if ($filter_attributes_exact === true && count($attributes) !== count($filter_attributes)) {
 			return false;
