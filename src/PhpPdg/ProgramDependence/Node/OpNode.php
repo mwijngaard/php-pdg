48bd4ac9492b94b2193545acb08ceba009ad7cb5
Merijn Wijngaard <mwijngaard@gmail.com>
2016-06-15 15:30:13 +0200

    renamed op_node to op

--- src/PhpPdg/ProgramDependence/Node/OpNode.php
+++ src/PhpPdg/ProgramDependence/Node/OpNode.php
@@ -7,20 +7,20 @@ use PhpPdg\Graph\Node\AbstractNode;
 
 class OpNode extends AbstractNode {
 	/** @var Op  */
-	public $op_node;
+	public $op;
 
-	public function __construct(Op $op_node) {
-		$this->op_node = $op_node;
+	public function __construct(Op $op) {
+		$this->op = $op;
 	}
 
 	public function toString() {
-		$startLine = $this->op_node->getAttribute('startLine', -1);
-		$endLine = $this->op_node->getAttribute('endLine', -1);
+		$startLine = $this->op->getAttribute('startLine', -1);
+		$endLine = $this->op->getAttribute('endLine', -1);
 		$lines = $startLine === $endLine ? $startLine : $startLine . ':' . $endLine;
-		return sprintf('Op %s @ line %s', str_replace("PHPCfg\\Op\\", '', get_class($this->op_node)), $lines) ;
+		return sprintf('Op %s @ line %s', str_replace("PHPCfg\\Op\\", '', get_class($this->op)), $lines) ;
 	}
 
 	public function getHash() {
-		return 'OP(' . spl_object_hash($this->op_node) . ')';
+		return 'OP(' . spl_object_hash($this->op) . ')';
 	}
 }
