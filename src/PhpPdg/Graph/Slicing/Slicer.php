8e7935da56237e87d16ad8ec74c85177881e0585
Merijn Wijngaard <mwijngaard@gmail.com>
2016-06-16 10:36:18 +0200

    implemented seen nodes tracking using node hash

--- src/PhpPdg/Graph/Slicing/Slicer.php
+++ src/PhpPdg/Graph/Slicing/Slicer.php
@@ -3,17 +3,28 @@
 namespace PhpPdg\Graph\Slicing;
 
 use PhpPdg\Graph\GraphInterface;
+use PhpPdg\Graph\Node\NodeInterface;
 
 class Slicer implements SlicerInterface {
 	public function slice(GraphInterface $source, array $slicing_criterion, GraphInterface $target) {
-		$worklist = $slicing_criterion;
+		$nodes_seen = [];
+		$worklist = [];
+		/** @var NodeInterface $node */
+		foreach ($slicing_criterion as $node) {
+			$nodes_seen[$node->getHash()] = 1;
+			$worklist[] = $node;
+			$target->addNode($node);
+		}
 		while (empty($worklist) === false) {
 			$to_node = array_shift($worklist);
 			foreach ($source->getEdges(null, $to_node) as $incoming_edge) {
 				$from_node = $incoming_edge->getFromNode();
-				$target->addNode($from_node);
+				if (isset($nodes_seen[$from_node->getHash()]) === false) {
+					$nodes_seen[$from_node->getHash()] = 1;
+					$worklist[] = $from_node;
+					$target->addNode($from_node);
+				}
 				$target->addEdge($from_node, $to_node, $incoming_edge->getAttributes());
-				$worklist[] = $from_node;
 			}
 		}
 	}
