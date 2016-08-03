1dfe5b67bd7d03b9fd01e2cba7c79ac8c8edaeea
Merijn Wijngaard <mwijngaard@gmail.com>
2016-08-03 16:24:09 +0200

    removed toString from stop node, for consistency with entry node

--- src/PhpPdg/ProgramDependence/Node/StopNode.php
+++ src/PhpPdg/ProgramDependence/Node/StopNode.php
@@ -5,10 +5,6 @@ namespace PhpPdg\ProgramDependence\Node;
 use PhpPdg\Graph\Node\AbstractNode;
 
 class StopNode extends AbstractNode {
-	public function toString() {
-		return "Stop";
-	}
-
 	public function getHash() {
 		return 'STOP';
 	}
