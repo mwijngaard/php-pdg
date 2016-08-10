64941f8fc743730757946ddfeb8f85011d849855
Merijn Wijngaard <mwijngaard@gmail.com>
2016-08-10 13:16:45 +0200

    removed maybe data dependence generator from default factory implementation

--- src/PhpPdg/ProgramDependence/Factory.php
+++ src/PhpPdg/ProgramDependence/Factory.php
@@ -8,13 +8,11 @@ use PhpPdg\Graph\FactoryInterface as GraphFactoryInterface;
 use PhpPdg\Graph\Factory as GraphFactory;
 use PhpPdg\ProgramDependence\ControlDependence\GeneratorInterface as ControlDependenceGeneratorInterface;
 use PhpPdg\ProgramDependence\ControlDependence\Generator as ControlDependenceGenerator;
-use PhpPdg\ProgramDependence\DataDependence\CombiningGenerator;
 use PhpPdg\ProgramDependence\DataDependence\GeneratorInterface as DataDependenceGeneratorInterface;
 use PhpPdg\ProgramDependence\DataDependence\Generator as DataDependenceGenerator;
 use PhpPdg\ProgramDependence\ControlDependence\BlockFlowGraph\Generator as BlockCfgGenerator;
 use PhpPdg\ProgramDependence\ControlDependence\BlockDependenceGraph\Generator as BlockCdgGenerator;
 use PhpPdg\ProgramDependence\ControlDependence\PostDominatorTree\Generator as PdgGenerator;
-use PhpPdg\ProgramDependence\DataDependence\MaybeGenerator as MaybeDataDependenceGenerator;
 use PhpPdg\ProgramDependence\Node\EntryNode;
 use PhpPdg\ProgramDependence\Node\OpNode;
 
@@ -23,12 +21,23 @@ class Factory implements FactoryInterface {
 	private $control_dependence_generator;
 	private $data_dependence_generator;
 
+	/**
+	 * Factory constructor.
+	 * @param GraphFactoryInterface $graph_factory
+	 * @param ControlDependenceGeneratorInterface $control_dependence_generator
+	 * @param DataDependenceGeneratorInterface $data_dependence_generator
+	 */
 	public function __construct(GraphFactoryInterface $graph_factory, ControlDependenceGeneratorInterface $control_dependence_generator, DataDependenceGeneratorInterface $data_dependence_generator) {
 		$this->graph_factory = $graph_factory;
 		$this->control_dependence_generator = $control_dependence_generator;
 		$this->data_dependence_generator = $data_dependence_generator;
 	}
 
+	/**
+	 * @param CfgFunc $cfg_func
+	 * @param string $filename
+	 * @return Func
+	 */
 	public function create(CfgFunc $cfg_func, $filename = null) {
 		$pdg = $this->graph_factory->create();
 		$entry_node = new EntryNode();
@@ -50,16 +59,17 @@ class Factory implements FactoryInterface {
 		return $func;
 	}
 
+	/**
+	 * @param GraphFactoryInterface|null $graph_factory
+	 * @return Factory
+	 */
 	public static function createDefault(GraphFactoryInterface $graph_factory = null) {
 		$graph_factory = $graph_factory !== null ? $graph_factory : new GraphFactory();
 		$block_cfg_generator = new BlockCfgGenerator($graph_factory);
 		$block_cdg_generator = new BlockCdgGenerator($graph_factory);
 		$pdt_generator = new PdgGenerator($graph_factory);
 		$control_dependence_generator = new ControlDependenceGenerator($block_cfg_generator, $pdt_generator, $block_cdg_generator);
-		$data_dependence_generator = new CombiningGenerator([
-			new DataDependenceGenerator(),
-			new MaybeDataDependenceGenerator()
-		]);
+		$data_dependence_generator = new DataDependenceGenerator();
 		return new self($graph_factory, $control_dependence_generator, $data_dependence_generator);
 	}
 }
